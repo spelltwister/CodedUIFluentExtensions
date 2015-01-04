426a49db913add4f086314055e117adf3305d568
spelltwister <spelltwister@yahoo.com>
2015-01-04 17:07:00 -0500

    Adding no-login example site

---
+++ CodedUIExtensions/ExampleSite/Controllers/ExampleController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExampleSite.Controllers
+{
+    public class ExampleController : Controller
+    {
+        // GET: Example
+        public ActionResult Ex1()
+        {
+            return View("Ex1_SimpleForm");
+        }
+    }
+}
