88d9e6ac8c41a1b0527adb570eeb696bebd5b8b3
ashjslaughter <ashley.j.slaughter@gmail.com>
2015-04-02 14:53:13 -0400

    HtmlParagraph tag

---
+++ CodedUIExtensions/CodedUIExtensionsAndHelpers/AdditionalControls/Html/HtmlParagraph.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.VisualStudio.TestTools.UITesting;
+
+namespace CodedUIExtensionsAndHelpers.AdditionalControls.Html
+{
+    public class HtmlParagraph : HtmlCustomTag
+    {
+        public static readonly string ParagraphTag = "p";
+
+        public HtmlParagraph() : base(ParagraphTag) { }
+        public HtmlParagraph(UITestControl parent) : base(parent, ParagraphTag) { }
+    }
+}
