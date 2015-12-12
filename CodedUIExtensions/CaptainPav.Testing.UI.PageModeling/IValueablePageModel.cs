a6a33ef6b5ef93bcddf0b337d7321be2647ff357
Spelltwister <spelltwister@yahoo.com>
2015-12-12 15:13:28 -0500

    updated documentation

--- CodedUIExtensions/CaptainPav.Testing.UI.PageModeling/IValueablePageModel.cs
+++ CodedUIExtensions/CaptainPav.Testing.UI.PageModeling/IValueablePageModel.cs
@@ -12,6 +12,16 @@ namespace CaptainPav.Testing.UI.PageModeling
     /// </typeparam>
     public interface IValueablePageModel<TValue, out TNextModel> : IValuedPageModel<TValue> where TNextModel : IPageModel
     {
+        /// <summary>
+        /// Sets the values of this page model, if possible
+        /// </summary>
+        /// <param name="toValue">
+        /// The value to set into this page model
+        /// </param>
+        /// <returns>
+        /// The most likely model with which the user will interact after
+        /// setting this models value.
+        /// </returns>
         TNextModel SetValue(TValue toValue);
     }
 }
