3ff0db68c94de4a5760284f5ae7b19632a635104
Spelltwister <spelltwister@yahoo.com>
2016-01-26 16:25:09 -0500

    Added generic extension for waiting for a condition to be true for an element.  Also added an extension to ignore the out parameter from TryGetClickablePoint.

---
+++ CodedUIExtensions/CaptainPav.Testing.UI.CodedUI/CodedUITestControlExtensions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+using Microsoft.VisualStudio.TestTools.UITesting;
+
+namespace CaptainPav.Testing.UI.CodedUI
+{
+    public static class CodedUITestControlExtensions
+    {
+        /// <summary>
+        /// Blocks the current thread until the specified condition is met, or until the specified time-out expires.
+        /// </summary>
+        /// 
+        /// <returns>
+        /// true if the condition is met before the time-out; otherwise, false.
+        /// </returns>
+        /// <param name="conditionContext">The context to evaluate the condition.</param>
+        /// <param name="conditionEvaluator">The delegate to evaluate the condition.</param>
+        /// <param name="millisecondsTimeout">The number of milliseconds before time-out; if null, Playback.PlaybackSettings.WaitForReadyTimeout is used.</param>
+        /// <typeparam name="T">The <see cref="T:System.Type"/> that specifies the Type for the condition and predicate.</typeparam>
+        public static bool WaitForControlCondition<T>(this T conditionContext, Predicate<T> conditionEvaluator, int? millisecondsTimeout = null)
+        {
+            return UITestControl.WaitForCondition(conditionContext, conditionEvaluator, millisecondsTimeout ?? Playback.PlaybackSettings.WaitForReadyTimeout);
+        }
+
+        /// <summary>
+        /// Returns true if can clickable point can be found for this control
+        /// </summary>
+        /// <param name="control">
+        /// The control for which to find a clickable point
+        /// </param>
+        /// <returns>
+        /// True if can clickable point can be found for this control;
+        /// otherwise, false
+        /// </returns>
+        public static bool CanGetClickablePoint(this UITestControl control)
+        {
+            System.Drawing.Point p;
+            return control.TryGetClickablePoint(out p);
+        }
+    }
+}
