a7291ac99188f1a54f8fc76f98f98ee21b51cec6
Mike Pavlak <spelltwister@yahoo.com>
2016-01-27 22:47:20 -0500

    Added paragraph tag

---
+++ CodedUIExtensions/CaptainPav.Testing.UI.CodedUI/Html/AdditionalControls/HtmlParagraph.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UITesting;
+
+namespace CaptainPav.Testing.UI.CodedUI.Html.AdditionalControls
+{
+    public class HtmlParagraph : HtmlCustomTag
+    {
+        public static readonly string ParagraphTag = "p";
+
+        public HtmlParagraph() : base(ParagraphTag) { }
+
+        public HtmlParagraph(UITestControl parent) : base(parent, ParagraphTag) { }
+    }
+}
