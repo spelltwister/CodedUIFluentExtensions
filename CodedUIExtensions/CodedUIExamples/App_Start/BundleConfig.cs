c84659cd43e2f8296146a7da603b84033ef89e2e
Mike Pavlak <spelltwister@yahoo.com>
2016-03-13 16:30:55 -0400

    Added font awesome.  removed about page.  updated contact to include my twitter and info@visualchimera.com.  added text to the example 1 page comparing the code required to test the text input.  Added code to test the hidden input native style.

--- CodedUIExtensions/CodedUIExamples/App_Start/BundleConfig.cs
+++ CodedUIExtensions/CodedUIExamples/App_Start/BundleConfig.cs
@@ -25,7 +25,8 @@ namespace CodedUIExamples
 
 			bundles.Add(new StyleBundle("~/Content/css").Include(
 					  "~/Content/bootstrap.css",
-					  "~/Content/site.css"));
+					  "~/Content/site.css",
+                      "~/css/font-awesome.css"));
 		}
 	}
 }
--- CodedUIExtensions/Lib.Tests/CUIT/SimpleHtmlControlsTests.cs
+++ CodedUIExtensions/Lib.Tests/CUIT/SimpleHtmlControlsTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 using Microsoft.VisualStudio.TestTools.UITesting;
 using Microsoft.VisualStudio.TestTools.UITesting.HtmlControls;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -42,5 +43,24 @@ namespace Lib.Tests.CUIT
 			userNameEdit.Text = "MyUserName";
 			Assert.IsTrue(StringComparer.Ordinal.Equals(userNameEdit.Text, "MyUserName"));
 		}
-	}
+
+        [TestMethod]
+        public void TestHiddenInput()
+        {
+            HtmlDiv bodyContainerDiv = new HtmlDiv(this.window);
+            bodyContainerDiv.SearchProperties.Add(HtmlDiv.PropertyNames.Id, "layoutBodyContainer");
+
+            // no fieldset available
+            HtmlControl fieldset = new HtmlCustom(bodyContainerDiv);
+            fieldset.SearchProperties.Add(HtmlControl.PropertyNames.TagName, "fieldset");
+
+            HtmlEdit hiddenEdit = new HtmlEdit(fieldset);
+            hiddenEdit.SearchProperties.Add(HtmlEdit.PropertyNames.Id, "hiddenTextInput");
+
+            Assert.IsTrue(hiddenEdit.TryFind(), "Should be able to find the hidden input since it is in the markup.");
+
+            Point p;
+            Assert.IsFalse(hiddenEdit.TryGetClickablePoint(out p), "Should not be able to click on a hidden input.");
+        }
+    }
 }
